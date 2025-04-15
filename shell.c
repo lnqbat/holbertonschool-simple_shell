a3f214948027a3d4dd25f43c60ce66b037db968f
jeff <jeffrey-31@hotmail.fr>
2025-04-15 15:12:22 +0200

    add free ligne 42

--- shell.c
+++ shell.c
@@ -5,54 +5,48 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 /**
- *
- *
- *
+ * main - Super simple shell
+ * Return: ouille if close
  */
-
 int main(void)
 {
-        char *line = NULL;
-        size_t len = 0;
-        ssize_t nread;
-        pid_t pid;
-        char *argv[] = {line, NULL};
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	pid_t pid;
+	char *argv[] = {line, NULL};
 	int status;
 
+	while (1)
+	{
+		printf("#cisfun$ ");
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
+		{
+			perror("Ouille");
+			break;
+		}
+		if (line[nread - 1] == '\n')
+			line[nread - 1] = '\0';
 
-        while (1)
-        {
-                printf("#cisfun$ ");
-                nread = getline(&line, &len, stdin);
-
-                if (nread == -1)
-                {
-                        perror("Ouille");
-                        break;
-                }
-
-                if (line[nread - 1] == '\n')
-                line[nread - 1] = '\0';
-
-                pid = fork();
-                if (pid == -1)
-                {
-                        perror("Aie aie aie");
-                        break;
-                }
-                if (pid == 0)
-                {
-                        if (execve(line, argv, NULL) == -1)
-                        {
-                                perror("execve");
-                        }
-                        exit(1);
-                }
-                else
-                {
-                        wait(&status);
-                }
-        }
-        free(line);
-        return (0);
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("Aie aie aie");
+			break;
+		}
+		if (pid == 0)
+		{
+			if (execve(line, argv, NULL) == -1)
+				perror("execve");
+			free(line);
+			exit(1);
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
+	free(line);
+	return (0);
 }
