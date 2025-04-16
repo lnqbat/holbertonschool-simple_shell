516deb7062ca993537b91a35c21453d3cef25e4e
jeff <jeffrey-31@hotmail.fr>
2025-04-16 10:10:20 +0200

    delete line write and while loop useless, betty style

--- main.c
+++ main.c
@@ -8,29 +8,18 @@
 int main(void)
 {
 	char *line;
-	int i;
 	int is_prompt;
+
 	while (1)
 	{
 		is_prompt = isatty(STDIN_FILENO);
-		if(is_prompt == 1)
+		if (is_prompt == 1)
 			print_prompt();
 
 		line = read_input();
 		if (line == NULL)
-		{
-			write(STDOUT_FILENO, "\n", 1);
 			break;
-		}
 
-		i = 0;
-		while (line[i] == ' ' || line[i] == '\t' || line[i] == '\n')
-			i++;
-		if (line[i] == '\0')
-		{
-			free(line);
-			continue;
-		}
 		exec_command(line);
 		free(line);
 	}
