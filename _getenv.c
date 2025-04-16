3445746cb85433f5788cdcbfa12d0710940b4f27
lnqbat <b.lonqueu98@gmail.com>
2025-04-16 14:56:21 +0200

    add new function for search a PATH

---
+++ _getenv.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+/**
+ * _getenv - Get an environment variable
+ * @name: The name of the environment variable
+ * Return: environment variable
+ */
+
+char *_getenv(char *name)
+{
+	char **env = environ;
+	size_t len = strlen(name);
+
+	while (*env != NULL)
+	{
+		if (strncmp(*env, name, len) == 0 && (*env)[len] == '=')
+		{
+			return (*env);
+		}
+		env++;
+	}
+	return (NULL);
+}
+
