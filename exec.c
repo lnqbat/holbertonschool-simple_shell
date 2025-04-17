2aa85945ff2a733dab5f0b368aae2abd644f86d7
jeff <jeffrey-31@hotmail.fr>
2025-04-17 09:55:01 +0200

    update working branch

--- exec.c
+++ exec.c
@@ -9,12 +9,14 @@
 int exec_command(char *cmd)
 {
 	pid_t pid;
-	int status;
+	int status, i;
 	char **argv, *path_full;
 
 	argv = tokenize(cmd);
 	if (argv == NULL || argv[0] == NULL)
 	{
+		for (i = 0; argv[i]; i++)
+			free(argv[i]);
 		free(argv);
 		return (-1);
 	}
@@ -23,21 +25,28 @@ int exec_command(char *cmd)
 		path_full = _which(argv[0]);
 		if (path_full == NULL)
 		{
+			for (i = 0; argv[i]; i++)
+				free(argv[i]);
 			free(argv);
 			return (-1);
 		}
 		else
+		{
+			free(argv[0]);
 			argv[0] = path_full;
+		}
 	}
 	pid = fork();
 	if (pid == -1)
 	{
+		for (i = 0; argv[i]; i++)
+			free(argv[i]);
 		free(argv);
 		return (-1);
 	}
 	else if (pid == 0)
 	{
-		if (execve(argv[0], argv, environ) == -1)
+		if (execve(argv[0], argv, NULL) == -1)
 		{
 			perror("Error execve");
 			exit(EXIT_FAILURE);
@@ -45,6 +54,8 @@ int exec_command(char *cmd)
 	}
 	else
 		wait(&status);
+	for (i = 0; argv[i]; i++)
+		free(argv[i]);
 	free(argv);
 	return (0);
 }
--- tokenize.c
+++ tokenize.c
@@ -7,18 +7,31 @@
 
 char **tokenize(char *cmd)
 {
-	char **argv;
+	char **argv = NULL;
 	int bsize = 80, i = 0;
 	char *token;
 
 	argv = malloc(sizeof(char *) * bsize);
 	if (argv == NULL)
+	{
+		free(cmd);
 		return (NULL);
+	}
 
 	token = strtok(cmd, " \t\n");
-	while (token != NULL && i < bsize - 1)
+
+	while (token != NULL && i < bsize)
 	{
-		argv[i++] = token;
+		argv[i] = strdup(token);
+		
+		if (argv[i] == NULL)
+		{
+			free(cmd);
+			for (;i > 0; i--)
+				free(argv[i]);
+			free(argv);
+		}
+		i++;
 		token = strtok(NULL, " \t\n");
 	}
 	argv[i] = NULL;
