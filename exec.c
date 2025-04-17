49c776a31820c61d8c69c11be2c90c7690f2da47
jeff <jeffrey-31@hotmail.fr>
2025-04-17 22:38:14 +0200

    delete function have_slashed

--- exec.c
+++ exec.c
@@ -9,7 +9,7 @@
 int exec_command(char *cmd)
 {
 	pid_t pid;
-	int status, slashed;
+	int status;
 	char **argv, *path_full;
 
 	argv = tokenize(cmd);
@@ -18,8 +18,7 @@ int exec_command(char *cmd)
 		free_argv(argv);
 		return (-1);
 	}
-	slashed = have_slash(argv[0]);
-	if (slashed == 0)
+	if (access(argv[0], F_OK) == -1)
 	{
 		path_full = _which(argv[0]);
 		if (path_full == NULL)
@@ -35,29 +34,19 @@ int exec_command(char *cmd)
 			argv[0] = path_full;
 		}
 	}
-	if (access(argv[0], F_OK) == 0)
+	pid = fork();
+	if (pid == -1)
 	{
-		pid = fork();
-		if (pid == -1)
-		{
-			free_argv(argv);
-			return (-1);
-		}
-		else if (pid == 0)
-		{
-			if (execve(argv[0], argv, environ) == -1)
-				exit(EXIT_FAILURE);
-		}
-		else
-			waitpid(pid, &status, 0);
+		free_argv(argv);
+		return (-1);
+	}
+	else if (pid == 0)
+	{
+		if (execve(argv[0], argv, environ) == -1)
+			exit(EXIT_FAILURE);
 	}
 	else
-	{
-		fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
-		free_argv(argv);
-		free(cmd);
-		exit(127);
-	}
+		    waitpid(pid, &status, 0);
 	free_argv(argv);
 	return (0);
 }
