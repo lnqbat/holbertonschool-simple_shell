e17f146de24d37a5770583cd6173c3c2865970bb
lnqbat <b.lonqueu98@gmail.com>
2025-04-18 18:11:36 +0200

    add prototype new function exit

--- shell.h
+++ shell.h
@@ -7,8 +7,10 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <string.h>
+
 #define INT_MIN (-2147483648)
 #define INT_MAX (2147483647)
+
 extern char **environ;
 
 void print_prompt(void);
@@ -17,7 +19,9 @@ char *read_input(void);
 char **tokenize(char *cmd);
 char *_getenv(char *name);
 int free_argv(char **argv);
+int _atoi(char *s);
+int exit_command(char **argv);
+
 int exec_command(char *cmd);
 char *_which(const char *filename);
-int _atoi(char *s);
 #endif
