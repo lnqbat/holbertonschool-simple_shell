f4f88320da7248ebc27372c37a5484bc0ce4ebfb
lnqbat <b.lonqueu98@gmail.com>
2025-04-18 15:10:13 +0200

    add line 24 function that exit shell

--- exec.c
+++ exec.c
@@ -11,28 +11,37 @@ int exec_command(char *cmd)
 	int status;
 	char **argv, *path_full;
 
-
+	if (strcmp(cmd, "exit"))
+	{
+		exit(EXIT_SUCCESS);
+	}
 	argv = tokenize(cmd);
 	if (argv == NULL || argv[0] == NULL)
 	{
 		free_argv(argv);
 		return (-1);
 	}
+	if (strcmp(argv[0], "exit") == 0)
+        {
+		free_argv(argv);
+		free(cmd);
+                exit(EXIT_SUCCESS);
+        }
 	if (!strchr(argv[0], '/'))
 	{
-			path_full = _which(argv[0]);
-			if (path_full == NULL)
-			{
-				fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
-				free_argv(argv);
-				free(cmd);
-				exit(127);
-			}
-			else
-			{
-				free(argv[0]);
-				argv[0] = path_full;
-			}
+		path_full = _which(argv[0]);
+		if (path_full == NULL)
+		{
+			fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
+			free_argv(argv);
+			free(cmd);
+			exit(127);
+		}
+		else
+		{
+			free(argv[0]);
+			argv[0] = path_full;
+		}
 	}
 
 	if (access(argv[0], F_OK) == -1)
