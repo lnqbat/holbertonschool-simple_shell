36a2bc8574be71ec242a517c7e7ddb472e6d2261
jeff <jeffrey-31@hotmail.fr>
2025-04-20 14:56:18 +0200

    keep instruction to get path in new function

--- exec.c
+++ exec.c
@@ -3,40 +3,16 @@
 /**
  * exec_command - execute command
  * @argv: command tokenised to execute
- * @line: need to free if necessaire
  * @last_status: pointer to keep the status of processus children
  * Return: 0 on success,
  */
-int exec_command(char **argv, char *line, int *last_status)
+int exec_command(char **argv, int *last_status)
 {
 	pid_t pid;
 	int status;
-	char *path_full;
 
-	if (!strchr(argv[0], '/'))
-	{
-		path_full = _which(argv[0]);
-		if (path_full == NULL)
-		{
-			fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
-			free_argv(argv);
-			free(line);
-			exit(127);
-		}
-		else
-		{
-			free(argv[0]);
-			argv[0] = path_full;
-		}
-	}
-	if (access(argv[0], X_OK) == -1)
-	{
-		fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
-		free_argv(argv);
-		free(line);
-		exit(127);
-	}
 	pid = fork();
+
 	if (pid == -1)
 	{
 		free_argv(argv);
