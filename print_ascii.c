87fba3d64821cac932fb7089ceb28a80ba55d5c4
lnqbat <b.lonqueu98@gmail.com>
2025-04-23 15:10:24 +0200

    add function that print ascii in the shell

---
+++ print_ascii.c
@@ -0,0 +1,87 @@
+#include "shell.h"
+
+/**
+ * print_ascii - Print hugo in terminal.
+ * @argv: unusable.
+ * @last_status: unusable.
+ * Return: 0 .
+ */
+int print_ascii(char **argv, int *last_status)
+{
+	(void) argv;
+	(void) last_status;
+
+	printf("\nThe Best SWE\n");
+
+	printf("-=-=========*+-+##*+#####+*##*====-=#################*####\n");
+	printf("-==============*#*==+==-:...::     .::-==*##########*#*===\n");
+	printf("-===============+=--:..                 .-++*######***====\n");
+	printf("-==============-:.      .::::-===---.     .:-++*#####*-===\n");
+	printf("=============--.      .-=====+******++:      ..-*****+====\n");
+	printf("===========-:.       .-=+*************=.       .==---=====\n");
+	printf("====-====-=.       .:--:.::--+***-.::.:-.       .:-=======\n");
+	printf("=========-.        .        :---:     ..          .-======\n");
+	printf("=======--:          .----:. :---:..----:.           .:====\n");
+	printf("-=====-:           .......:::=+--::.......      .    -====\n");
+	printf("--=-==.           ::....:----=+---==.  ::::.         -====\n");
+	printf("-====-          .:---------..::::-+*===--=-.         .-===\n");
+	printf("--===-.         .------+=:.       :-+***+--.           -==\n");
+	printf("-===--.         .:--------.      .---------.          :--=\n");
+	printf("-==-=:          ..----:::..:..:..:..::-:---.         :====\n");
+	printf("--=---          ..----. .........:..  :---:        ...-===\n");
+	printf("--===-:         ..:---:::.:::::.:-:::-----:.     .:--=+===\n");
+	printf("----=--..        ....::-----:..::------:::.    .:---=-====\n");
+	printf("-------=:         .....:--------------:...     ...----==-=\n");
+	printf("---------....        ...:::::-:::-::::...       ...----==-\n");
+	printf("-------::...                     .     ..        ..:------\n");
+	printf("------::...                          ....        ...:-----\n");
+	printf("------....                          .....  .    ....:-----\n");
+	printf("---=:.:...     ...                  ..... .::   ....:-----\n");
+	printf("-----::-----:-----: ...           .....:-:--=*-=+-::.:----\n");
+	return (0);
+}
+
+/**
+ * print_ascii_2 - Print fabien in terminal
+ * @argv: unusable.
+ * @last_status: unusable.
+ * Return: 0 .
+ */
+
+int print_ascii_2(char **argv, int *last_status)
+{
+	(void) argv;
+	(void) last_status;
+
+	printf("\nThe Best SWE\n");
+
+
+	printf("_..-___                                             ____.|\n");
+	printf(":-   _;j|||====:                                      ___.\n");
+	printf("::-_jjDDDDDDDDDD==__     __,....,`-=_                :====\n");
+	printf("-  ;jDDDDDDRRR!!^^'`  _|=|[|||||====:-_              :====\n");
+	printf("== DDDDDDRR^  ____  _.===[||RjRRRR|U==:.__           |====\n");
+	printf(" ,jDDDRBRH _j#K@@DDH=||||[jRDDDDDDDR|U==:-_          |====\n");
+	printf("::--DRBRH _#B@BRRDR==||jRRRDDDDDDDDDRRU=::-          |====\n");
+	printf(":,-DRR@D  jB@@BRRDD|||[R[RRDDDDDDDDDRRU==::          |====\n");
+	printf("=,DDRRR^  [RRRRRDRR=|||RRRDDDDDDRRRR^!U==,           |==||\n");
+	printf("=:DRU.   `--![RRRRRH^! _  --=RRRU==   -_.`  =        ||||=\n");
+	printf(":,-=----`   -'[RDD@U||=.[,,U= =m|jDU;U=||__ :        [[[[[\n");
+	printf(":-:--@@@D@@@@@@DDDDH,[jRDDRRHj[U=1RRRRRRR|- `        [||||\n");
+	printf(":--:[[U^ ![RRRRRDDRH[=jj[2j=|jRRDH=g==w=|=:          [||||\n");
+	printf(" |||[RU   jRRRRRRRRH![[RRRRH''^!![RRRRRR|=.          [[[[|\n");
+	printf(":--|||H   jRRRRRR[[R-`jRH`  =====._`!!RU'           :jRRRR\n");
+	printf(":--:=[H   [|||||||||U    [RRRRRRRDDDH:`             |jRRRR\n");
+	printf(":` '=|=   |||||||||||U   =RRU.__jRR|U               |jRRRR\n");
+	printf("-    |=   ||||||||||||U._`|j|=|U=|U=`  _  __        |jRRRR\n");
+	printf(":-  -'-   |========RH` ====!!=-==!^___ ;=.=`        'jRdmm\n");
+	printf("   --|    =]'   `     |=|===;:;,,:=:jrD:=`       DRRRRU---\n");
+	printf("-                     U=|[jDDjjDRDDDDH`           ^DR=,--D\n");
+	printf("-                     j|=[jRRRRDRR^                 -:|jDD\n");
+	printf("-   _-                                                --|j\n");
+	printf("-    `                                                 |[R\n");
+	printf("-                                                        R\n");
+	printf("-	^===                       ^P%%71+jj~-,R          \n");
+
+	return (0);
+}
