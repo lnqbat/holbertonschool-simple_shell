5c51a2cf0da9ff48910c3d9428859e5f8c032de4
lnqbat <b.lonqueu98@gmail.com>
2025-04-23 15:59:59 +0200

    add help builtin function in shell

---
+++ help.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+/**
+ * help_command - hep function that help
+ * @argv: not used
+ * @last_status: not used
+ * Return: 0 .
+ */
+
+int help_command(char **argv, int *last_status)
+{
+	(void) argv;
+	(void) last_status;
+
+	printf("Shell Help:\n");
+	printf("Available built-in commands:\n");
+	printf("  exit    - Exit the shell.\n");
+	printf("  env     - Display the environment variables.\n");
+	printf("  help    - Display information about built-in commands.\n");
+	printf("  Otherwise, manage on your own !\n");
+
+	return (0);
+}
